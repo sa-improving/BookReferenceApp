4b822a26caea2e29ff7d5b457b58e3d374a0e267
Skyler Aschenbeck <Skyler.Aschenbeck@improving.com>
2021-05-20 17:03:04 -0500

    Established SQL connection and display items from database on views

--- BookReference/Controllers/HomeController.cs
+++ BookReference/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using BookReference.Models;
+using BookReference.ViewModels.Home;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -20,7 +22,42 @@ namespace BookReference.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var Books = new List<Book>();
+
+            //Note: DO NOT EVER HARDCODE CONNECTION LIKE THIS vvvv
+            using (var conn = new SqlConnection("Server = .; Database = Books; Trusted_Connection = True;"))
+            {
+                conn.Open();
+
+                var cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.CommandText = "SELECT * FROM Books";
+
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var title = reader["Title"].ToString();
+                    var id = Convert.ToInt32(reader["BookId"]);
+
+                    //create my book here
+                    Books.Add(new Book
+                    {
+                        Title = title,
+                        Id = id
+                    }
+                    );
+
+                }
+
+            }
+
+            var vm = new HomeViewModel();
+            vm.Message = "Look at these wonderful books!";
+            vm.Books = Books;
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ BookReference/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookReference.Models;
+
+namespace BookReference.ViewModels.Home
+{
+    public class HomeViewModel
+    {
+        public string Message { get; set; }
+
+        public List<Book> Books { get; set; }
+    }
+}
